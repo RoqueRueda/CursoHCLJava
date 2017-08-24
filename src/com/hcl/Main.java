49335ccd2f9385160dd0fac803dbf1352f4a05bd
erikquiroz <emmanuel_quiroz@live.com.mx>
2017-08-24 16:11:57 -0500

    Initial commit

---
+++ src/com/hcl/Main.java
@@ -0,0 +1,15 @@
+package com.hcl;
+
+public class Main {
+	
+	String name = "Roque";
+
+	public static void main(String[] args) {
+		
+		Main m = new Main();
+		
+		System.out.println(m.name + " Hello World!!");
+
+	}
+
+}
